14e87c43be61820bd76714e35e3840811b7a98f6
rusakkate <melitrica_red@mail.ru>
2022-07-23 17:26:54 +0300

    Initial commit

---
+++ src/main/java/by/rusak/Test.java
@@ -0,0 +1,12 @@
+package by.rusak;
+
+public class Test {
+    public static void main(String[] args) {
+        int x, y;
+        x = 5;
+        y = 7;
+        int sum;
+        sum = x + y;
+
+    }
+}
