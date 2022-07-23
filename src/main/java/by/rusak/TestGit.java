d9523d30edcf96a1e32f8605f06c2112d973ea97
rusakkate <melitrica_red@mail.ru>
2022-07-23 17:37:32 +0300

    test new branch by rebase Change code

--- src/main/java/by/rusak/TestGit.java
+++ src/main/java/by/rusak/TestGit.java
@@ -3,6 +3,6 @@ package by.rusak;
 public class TestGit {
     public static void main(String[] args) {
         String string1 = "Hello";
-        String string2 = "Hello Git";
+        String string3 = "Hello GitRebase";
     }
 }
