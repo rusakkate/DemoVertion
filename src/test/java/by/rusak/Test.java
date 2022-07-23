89d91b26023de83e008331332192ae3ce26ac928
rusakkate <melitrica_red@mail.ru>
2022-07-23 17:34:57 +0300

    test new branch by rebase Add class

---
+++ src/test/java/by/rusak/Test.java
@@ -0,0 +1,11 @@
+package by.rusak;
+
+import java.util.Random;
+
+public class Test {
+    public static void main(String[] args) {
+        int x;
+        Random rand = new Random();
+        x = rand.nextInt();
+    }
+}
