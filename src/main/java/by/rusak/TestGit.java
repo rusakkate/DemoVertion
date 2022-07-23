ff92d8bef197503227217d648e58574d1c8f890f
rusakkate <melitrica_red@mail.ru>
2022-07-23 17:31:42 +0300

    test new branch by merge Change code

--- src/main/java/by/rusak/TestGit.java
+++ src/main/java/by/rusak/TestGit.java
@@ -2,6 +2,7 @@ package by.rusak;
 
 public class TestGit {
     public static void main(String[] args) {
-        String string = "Hello";
+        String string1 = "Hello";
+        String string2 = "Hello Git";
     }
 }
